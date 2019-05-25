07afd27085b6e8d465b3a28c6875ffa6fe55d9d9
jeffersonkr <jeffersonkr@hotmail.com>
2019-05-24 23:53:24 -0300

    add accounts url

--- luizalabs_employee_manager/urls.py
+++ luizalabs_employee_manager/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('employee/', include('api.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
