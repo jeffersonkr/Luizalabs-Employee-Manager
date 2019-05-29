4ce4796cc6e21c26bcb6c3f574c8488d03e6660c
jeffersonkr <jeffersonkr@hotmail.com>
2019-05-28 22:17:24 -0300

    DEBUG to False and ALLOWED HOST all

--- luizalabs_employee_manager/settings.py
+++ luizalabs_employee_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a@z%&z($h&h=du_!qnnmi%j%rnie-@+f9m=c$gr8fnbg)v=(32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
